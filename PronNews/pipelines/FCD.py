8eded13aacf1aa34447c36073542a2968138cacb
pn <null>
2022-04-30 14:06:40 +0800

    fix: sync

--- PronNews/pipelines/FCD.py
+++ PronNews/pipelines/FCD.py
@@ -1,40 +1,45 @@
 import datetime
 
-from twisted.enterprise import adbapi
+import pymysql
+from scrapy import Spider
+
+from PronNews.utils import get_snowflake_uuid
 
 
 class Pipeline(object):
 
-    def __init__(self, db_pool):
-        self.db_pool = db_pool
-
-    @classmethod
-    def from_settings(cls, settings):
-        db_params = settings.get('MYSQL')
-        db_pool = adbapi.ConnectionPool('pymysql', **db_params)
-        return cls(db_pool)
-
-    # 数据过滤
-    def filer_item(self, result, item, spider):
-        if item['create_date'] is not None:
-            sql = "UPDATE video SET screenshot = '%s',thumb = '%s',author = '%s',author_home = '%s'" \
-                  ",tags = '%s',create_date= '%s',update_time = '%s',product = '%s' WHERE id = '%s'"
-            par = (item['screenshot'], item['thumb'], item['author'], item['author_home'], item['tags'],
-                   item['create_date'], datetime.datetime.now(), item['product'], result[0][0])
-            if 'url' in item and item['url'] is not None:
-                self.db_pool.runQuery("INSERT INTO redirect(vid,url) VALUES('%s','%s')" % (item['vid'], item['url']))
-        else:
-            sql = "UPDATE video SET state = -1 WHERE id = '%s'"
-            par = result[0][0]
-        state = self.db_pool.runQuery(sql % par)
-        state.addErrback(self.handle_error, item, spider)
-
-    # 错误处理
-    @staticmethod
-    def handle_error(failure, item, spider):
-        print(failure)
+    def __init__(self, spider: Spider):
+        config = spider.settings.get('MYSQL')
+        self.connect = pymysql.connect(host=config['host'], user=config['user'], passwd=config['passwd'],
+                                       db=config['db'])
+        self.cursor = self.connect.cursor()
 
     def process_item(self, item, spider):
-        query = self.db_pool.runQuery("SELECT id from video WHERE vid = '%s'" % item['vid'])
-        query.addCallback(self.filer_item, item, spider)
-        query.addErrback(self.handle_error, item, spider)
+        result = self.cursor.execute("SELECT id,create_date from video WHERE vid = '%s'" % item['vid'])
+        _id = result[0][0]
+        create_date = result[0][1]
+        if item['create_date'] is not None and create_date is None:
+            pq = self.cursor.execute("SELECT id FROM product WHERE name = '%s'" % item['product'])
+            pid = pq[0][0]
+            if pq is None:
+                now = datetime.datetime.now()
+                pid = get_snowflake_uuid()
+                sql = """
+                INSERT INTO product(id,name,home,create_time,update_time,state) 
+                value('%s','%s','%s','%s','%s')
+                """
+                self.cursor.execute(
+                    sql % (pid, item['product'], item['product_home'], now, now, 1))
+                self.connect.commit()
+            self.cursor.execute(
+                "UPDATE video SET screenshot = '%s',thumb = '%s',tags = '%s',create_date= '%s',update_time = '%s'" \
+                ",pid = '%s' WHERE id = '%s'" % (item['screenshot'], item['thumb'], item['tags'], item['create_date'],
+                                                 datetime.datetime.now(), pid, _id))
+            self.connect.commit()
+        elif item['create_date'] is None:
+            self.cursor.execute("UPDATE video SET state = -1 WHERE id = '%s'" % _id)
+            self.connect.commit()
+
+    def close_spider(self, spider):
+        self.cursor.close()
+        self.connect.close()
