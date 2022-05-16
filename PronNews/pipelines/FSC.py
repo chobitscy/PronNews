2087e51416d9bdff6a7e01d42a3ace6d00677899
pn <null>
2022-05-16 23:34:07 +0800

    fix: FSC

--- PronNews/pipelines/FSC.py
+++ PronNews/pipelines/FSC.py
@@ -27,8 +27,9 @@ class Pipeline(object):
         response = requests.get(url)
         requests.post(self.update_api, files={'file': response.content},
                       headers={'Authorization': self.auth}, data={'name': image_name})
-        self.session.query(Video).filter(Video.id == item['id']).update(
-            {Video.print_screen: Video.print_screen + image_name})
+        sql = 'UPDATE video SET print_screen = IFNULL(CONCAT(print_screen,%s),%s) WHERE id = %s' % \
+              (',' + image_name, image_name, item['id'])
+        self.session.execute(sql)
         self.session.commit()
 
     def close_spider(self, spider):
