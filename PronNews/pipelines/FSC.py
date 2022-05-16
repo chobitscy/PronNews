270257644c8a0d8cb141ce355cb45a32f086f590
pn <null>
2022-05-17 00:51:19 +0800

    feat: imgblaze

--- PronNews/pipelines/FSC.py
+++ PronNews/pipelines/FSC.py
@@ -1,3 +1,4 @@
+import datetime
 import re
 
 import requests
@@ -5,7 +6,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from PronNews import settings
-from PronNews.model.video import Video
 
 
 class Pipeline(object):
@@ -23,12 +23,15 @@ class Pipeline(object):
 
     def process_item(self, item, spider):
         url = item['print_screen']
+        if url is None:
+            return
+        update_time = datetime.datetime.now()
         image_name = re.findall(r'/FC2-PPV-(.*?).jpg', url)[0] + '.jpg'
         response = requests.get(url)
         requests.post(self.update_api, files={'file': response.content},
                       headers={'Authorization': self.auth}, data={'name': image_name})
-        sql = "UPDATE video SET print_screen = IFNULL(CONCAT(print_screen,'%s'),'%s') WHERE id = %s" % \
-              (',' + image_name, image_name, item['id'])
+        sql = "UPDATE video SET print_screen = IFNULL(CONCAT(print_screen,'%s'),'%s'), update_time = '%s' " \
+              "WHERE id = %s" % (',' + image_name, image_name, update_time, item['id'])
         self.session.execute(sql)
         self.session.commit()
 
--- PronNews/spiders/FSC.py
+++ PronNews/spiders/FSC.py
@@ -40,7 +40,9 @@ class FSC(RedisSpider, DataMixin):
         soup = BeautifulSoup(response.text, 'lxml')
         selector = soup.select('#torrent-description')[0].get_text().split('\n')
         for text in selector:
-            if text != '' and len(re.findall(r'https://(.*?)_s.jpg.html', text)) != 0:
+            if text != '':
+                continue
+            if len(re.findall(r'https://(.*?)_s.jpg.html', text)) != 0:
                 domain = re.findall(r'https://(.*?)/', text)[0]
                 _id = re.findall(r'https://%s/(.*?)/' % domain, text)[0]
                 data = {
@@ -49,9 +51,17 @@ class FSC(RedisSpider, DataMixin):
                     'pre': '1',
                     'next': 'Continue to image...'
                 }
-                yield scrapy.FormRequest(text, callback=self.parse_real, formdata=data, meta=response.meta)
+                yield scrapy.FormRequest(text, callback=self.cn, formdata=data, meta=response.meta)
 
-    def parse_real(self, response):
+            pattern = re.findall(r'(http|https)://(.*?)/', text)
+            if len(pattern) == 0:
+                continue
+            domain = pattern[0][1]
+            if domain == 'imgblaze.net' or domain == 'imgfrost.net':
+                text.replace(domain, 'imgair.net')
+                yield scrapy.Request(url=text, callback=self.imgair, meta=response.meta)
+
+    def cn(self, response):
         target = response.meta['target']
         item = Video()
         item['id'] = target['id']
@@ -59,6 +69,24 @@ class FSC(RedisSpider, DataMixin):
         try:
             src = re.findall(r'<img src="(.*?)" class="pic"', response.text)[0]
             item['print_screen'] = src
+        except IndexError:
+            item['print_screen'] = None
+        yield item
+
+    def imgair(self, response):
+        target = response.meta['target']
+        item = Video()
+        item['id'] = target['id']
+        item['vid'] = target['vid']
+        try:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            pattern = re.compile(r"document.getElementById\(\"newImgE\"\)\.src = \"https://prcf.(.*?);$",
+                                 re.MULTILINE | re.DOTALL)
+            soup_document = soup.find_all('script', text=pattern)[0]
+            pattern = re.compile(
+                r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+            url = re.findall(pattern, str(soup_document))[0]
+            item['print_screen'] = url
         except IndexError:
             item['print_screen'] = None
         yield item
