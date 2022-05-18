8bed653b57e6f34a559417273cb5788fe566fd41
pn <null>
2022-05-18 22:29:09 +0800

    feat: FSC

--- PronNews/spiders/FSC.py
+++ PronNews/spiders/FSC.py
@@ -26,8 +26,7 @@ class FSC(RedisSpider, DataMixin):
 
     def __init__(self, *args, **kwargs):
         super().__init__()
-        sql = "SELECT id,vid,cid FROM video WHERE print_screen IS NULL AND state = 1" \
-              " AND pub_date BETWEEN DATE_SUB(NOW(),INTERVAL 1 DAY) AND NOW()"
+        sql = "SELECT id,vid,cid FROM video WHERE print_screen IS NULL"
         super().custom(sql)
         super().push(self.redis_key, self.results, cid=True)
 
